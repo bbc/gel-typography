bbd0dab0ea18b297d7ecddd67ee9311162c842d7
Matthew Crouch <matthew.crouch@bbc.co.uk>
2016-09-23 14:32:46 +0100

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -13,10 +13,12 @@ git config --global user.name "Travis CI"
 git config --global user.email "$EMAIL"
 
 npm run dist
-mv dist/gel-typography.css gel-grid.css/gel-typography.css
-mv dist/gel-typography.min.css gel-grid.css/gel-typography.min.css
+mv dist/gel-typography.css gel-typography.css/gel-typography.css
+mv dist/gel-typography.min.css gel-typography.css/gel-typography.min.css
 
 cd gel-typography.css
 git add -A .
 git commit -m "Built dist from bbc/gel-typography $TRAVIS_COMMIT"
 git push -q https://$GH_TOKEN@github.com/bbc/gel-typography.css.git master
+cd ..
+git stash
